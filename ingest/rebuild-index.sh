51c369ac6c9b80d9a414f2f2db89f9d89776512d
Don Elsborg <elsborg@colorado.edu>
2017-09-08 15:12:39 -0600

    rebuild-index script tool

---
+++ ingest/rebuild-index.sh
@@ -0,0 +1,7 @@
+# get counts of index prior to deletion
+./idx_get_count.sh > idx_counts-prior.out
+curl -XDELETE localhost:9200/fis
+curl -XPUT localhost:9200/fis
+curl -XPUT 'localhost:9200/fis/person/_mapping?pretty' --data-binary @mappings/person.json
+curl -XPUT 'localhost:9200/fis/equipment/_mapping?pretty' --data-binary @mappings/equipment.json
+curl -XPOST 'localhost:9200/_bulk' --data-binary @p.out
