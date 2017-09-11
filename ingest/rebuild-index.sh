492bdff284725bb24b92c56409bd7a80f0e61030
Elsborg Donald <elsborg@prometheus.int.colorado.edu>
2017-09-11 15:38:00 -0600

    only ingest people with vcard fixes

--- ingest/rebuild-index.sh
+++ ingest/rebuild-index.sh
@@ -4,4 +4,4 @@ curl -XDELETE localhost:9200/fis
 curl -XPUT localhost:9200/fis
 curl -XPUT 'localhost:9200/fis/person/_mapping?pretty' --data-binary @mappings/person.json
 curl -XPUT 'localhost:9200/fis/equipment/_mapping?pretty' --data-binary @mappings/equipment.json
-curl -XPOST 'localhost:9200/_bulk' --data-binary @p.out
+curl -XPOST 'localhost:9200/_bulk' --data-binary @people-fix.list
