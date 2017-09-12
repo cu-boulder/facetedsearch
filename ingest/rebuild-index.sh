53c01548338f88a8c40023cb5a9d7cdb32bd7779
Don Elsborg <elsborg@colorado.edu>
2017-09-12 14:09:25 -0600

    indexed orcid field, optimized rebuild-index.sh

--- ingest/rebuild-index.sh
+++ ingest/rebuild-index.sh
@@ -2,14 +2,15 @@
 dstamp=`date +%Y%m%e-%H%M%S`
 mkdir spool/$dstamp
 outfile="spool/${dstamp}/rebuild-index.out"
-echo "Index counts prior to run" > $outfile
+echo "CREATING ES DOCUMENTS" > $outfile
+python ./ingest-people.py spool/$dstamp/people.list  >> $outfile 2>&1
+python ./ingest-equipment.py spool/$dstamp/equipment.list >> $outfile 2>&1
+echo "Index counts prior to run" >> $outfile
 ./idx_get_count.sh >> $outfile
 curl -XDELETE localhost:9200/fis >> $outfile
 curl -XPUT localhost:9200/fis >> $outfile
 curl -XPUT 'localhost:9200/fis/person/_mapping?pretty' --data-binary @mappings/person.json >> $outfile
 curl -XPUT 'localhost:9200/fis/equipment/_mapping?pretty' --data-binary @mappings/equipment.json >> $outfile
-python ./ingest-people.py spool/$dstamp/people.list  >> $outfile 2>&1
-python ./ingest-equipment.py spool/$dstamp/equipment.list >> $outfile 2>&1
 curl -XPOST 'localhost:9200/_bulk' --data-binary @spool/$dstamp/people.list >> $outfile 2>&1
 curl -XPOST 'localhost:9200/_bulk' --data-binary @spool/$dstamp/equipment.list >> $outfile 2>&1
 sleep 10
