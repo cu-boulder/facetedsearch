4491ce5ec43fef8f9763b64d5fb655389549b046
Elsborg Donald <elsborg@prometheus.int.colorado.edu>
2018-03-07 23:12:54 -0700

    rc1 release

---
+++ ingest/rebuild-pubs.sh
@@ -0,0 +1,22 @@
+# get counts of index prior to deletion
+indexname=fispubs-v1
+dstamp=`date +%Y%m%d-%H%M%S`
+outdir="spool/${dstamp}"
+mkdir $outdir
+outfile="${outdir}/rebuild-pubs.out"
+echo "CREATING ES DOCUMENTS" > $outfile
+python ./ingest-publications.py --spooldir ${outdir} ${outdir}/allpubs.idx  >> $outfile 2>&1
+echo "Index counts prior to run" >> $outfile
+#./idx_get_count.sh >> $outfile
+curl -XDELETE localhost:9200/${indexname} >> $outfile
+curl -XPUT localhost:9200/${indexname} >> $outfile
+curl -XPUT localhost:9200/${indexname}/publication/_mapping?pretty --data-binary @mappings/publication.json >> $outfile
+for f in $outdir/idx-*
+do 
+   echo $f 
+   curl -XPUT 'localhost:9200/_bulk' --data-binary @$f >> $outfile 2>&1
+done
+
+sleep 10
+echo "Index counts after run" >> $outfile
+#./idx_get_count.sh >> $outfile 2>&1
