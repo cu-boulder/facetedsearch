4335323689814c3e77eb820282f34093442ff3fb
Elsborg Donald <elsborg@prometheus.int.colorado.edu>
2018-03-08 15:58:08 -0700

    code cleanup for pull request

--- ingest/rebuild-pubs.sh
+++ ingest/rebuild-pubs.sh
@@ -7,7 +7,7 @@ outfile="${outdir}/rebuild-pubs.out"
 echo "CREATING ES DOCUMENTS" > $outfile
 python ./ingest-publications.py --spooldir ${outdir} ${outdir}/allpubs.idx  >> $outfile 2>&1
 echo "Index counts prior to run" >> $outfile
-#./idx_get_count.sh >> $outfile
+./idx_get_count.sh $indexname >> $outfile
 curl -XDELETE localhost:9200/${indexname} >> $outfile
 curl -XPUT localhost:9200/${indexname} >> $outfile
 curl -XPUT localhost:9200/${indexname}/publication/_mapping?pretty --data-binary @mappings/publication.json >> $outfile
@@ -19,4 +19,4 @@ done
 
 sleep 10
 echo "Index counts after run" >> $outfile
-#./idx_get_count.sh >> $outfile 2>&1
+./idx_get_count.sh $indexname >> $outfile 2>&1
