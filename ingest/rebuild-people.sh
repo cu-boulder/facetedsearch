77e6c4844e333ea40a2bda740d31489fc69e810a
Elsborg Donald <elsborg@prometheus.int.colorado.edu>
2019-01-17 13:00:52 -0700

    fixes for people

--- ingest/rebuild-people.sh
+++ ingest/rebuild-people.sh
@@ -1,15 +1,16 @@
 # get counts of index prior to deletion
 dstamp=`date +%Y%m%d-%H%M%S`
-dstamp="20190111-152317"
+#dstamp="20190111-152317"
 indexname=fis
-#mkdir spool/$dstamp
+mkdir spool/$dstamp
 outfile="spool/${dstamp}/rebuild-index.out"
-#echo "CREATING ES DOCUMENTS" > $outfile
-#python ./ingest-people.py spool/$dstamp/people.list  # >> $outfile 2>&1
+echo "CREATING ES DOCUMENTS" > $outfile
+python ./ingest-people.py spool/$dstamp/people.list   >> $outfile 2>&1
 #python ./ingest-equipment.py spool/$dstamp/equipment.list >> $outfile 2>&1
 #echo "Index counts prior to run" >> $outfile
 #./idx_get_count.sh >> $outfile
 ./idx_get_count.sh $indexname >> $outfile
+exit
 curl -XDELETE localhost:9200/fis >> $outfile
 curl -XPUT localhost:9200/fis >> $outfile
 curl -XPUT 'localhost:9200/fis/person/_mapping?pretty' --data-binary @mappings/person.json >> $outfile
