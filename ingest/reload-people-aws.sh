576fc7c08346539b755aff05f71acebda5328ee8
Donald R Elsborg <elsborg@cythna.colorado.edu>
2021-08-25 14:36:36 -0600

    Added people index to AWS

---
+++ ingest/reload-people-aws.sh
@@ -0,0 +1,14 @@
+# get counts of index prior to deletion
+. ./vivoapipw.py
+dstamp=20210825-101139
+outdir="spool/${dstamp}"
+indexname=$PEOPLEINDEX
+#mkdir spool/$dstamp
+outputfile="fispeople"
+logfile="spool/${dstamp}/rebuild-index.out"
+echo "CREATING ES DOCUMENTS" # > $logfile
+#./idx_get_count.sh ${indexname} #>> $logfile
+python load-data.py --spooldir ${outdir} --esendpoint ${PRODESENDPOINT} --esuser ${ESUSER} --espass ${ESPASS} --esservice ${ESSERVICE} --esregion ${ESREGION} --index ${indexname} --out $outputfile #DRE >> $logfile 2>&1
+sleep 10
+echo "Index counts after run" #>> $logfile
+./idx_get_count.sh #>> $logfile 2>&1
